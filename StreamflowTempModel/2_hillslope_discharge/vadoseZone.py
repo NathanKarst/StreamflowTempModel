5b175125aa4f99705642aa016edcde94082bc5e0
David Dralle <daviddralle@Davids-MacBook-Pro.local>
2017-02-05 21:35:45 -0800

    run 2/5

--- StreamflowTempModel/2_hillslope_discharge/vadoseZone.py
+++ StreamflowTempModel/2_hillslope_discharge/vadoseZone.py
@@ -297,10 +297,10 @@ class PreferentialRockMoistureZone(VadoseZone):
         sR = self.storageR/(self.nR*self.zrR)
         sS = self.storageS/(self.nS*self.zrS)
 
-        # frac = self.storageVZ/(self.nS*self.zrS + self.nR*self.zrR)
+        frac = self.storageVZ/(self.nS*self.zrS + self.nR*self.zrR)
 
-        # bypass = ppt*(frac)*self.alpha
-        # ppt = ppt - bypass
+        bypass = ppt*(frac)*self.alpha
+        ppt = ppt - bypass
 
         if (sS <= self.s0S):
             self.ETS = 0 
@@ -325,14 +325,14 @@ class PreferentialRockMoistureZone(VadoseZone):
         else: 
             self.ETR = (1-self.f)*pet
 
-        leakageToMatrix = soilLeakage*(1-self.alpha)
+        leakageToMatrix = soilLeakage#*(1-self.alpha)
 
         sR += leakageToMatrix*dt/(self.nR*self.zrR) - self.ETR*dt/(self.nR*self.zrR)
 
         #anything in excess of stS is drained to rock moisture
         self.leakage = np.max([sR - self.stR, 0])*self.nR*self.zrR/dt
         # add water preferentially routed through rock moisture zone
-        self.leakage += soilLeakage*self.alpha
+        self.leakage += bypass
 
         sR = np.min([sR, self.stR])
         self.storageR = sR*self.nR*self.zrR
