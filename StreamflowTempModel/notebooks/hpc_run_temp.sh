c23034f79e9f65b8d9ede6085184b9fb24227901
David Dralle <daviddralle@Davids-MacBook-Pro.local>
2017-02-06 23:42:10 -0800

    added temp hpc_run

---
+++ StreamflowTempModel/notebooks/hpc_run_temp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=test
+#
+# Account:
+#SBATCH --account=fc_hydrology
+#
+# Partition:
+#SBATCH --partition=savio2
+#
+# Wall clock limit:
+#SBATCH --time=24:00:00
+#
+## Command(s) to run:
+git checkout master
+source activate py2k_model
+python hpc_temperature_calibrate.py False 50000 elder_temperature.p elder.shp > output_elder_temperature.txt
