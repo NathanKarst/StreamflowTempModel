5d4c646d7eeb0672593ee25febf4280e280a4a0f
daviddralle <daviddralle@ucbvpn-208-155.vpn.berkeley.edu>
2017-09-15 17:00:29 -0700

    change to save text file output

--- StreamflowTempModel/notebooks/hpc_run.sh
+++ StreamflowTempModel/notebooks/hpc_run.sh
@@ -14,7 +14,7 @@
 ## Command(s) to run:
 git checkout master
 source activate py2k_model
-x=$(python hpc_hillslope_calibrate.py False 100 elder_runoff.p elder.shp)
+x=$(python hpc_hillslope_calibrate.py False 5 elder_runoff.p elder.shp)
 
 DATE=`date +%Y-%m-%d:%H:%M:%S`
 TITLESTR="ELDER_$DATE"
